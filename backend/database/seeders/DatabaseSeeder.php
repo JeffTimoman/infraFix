0f3b399ec4dcd21c9a067c319606662be68a87c9
JeffTimoman <ppti.jeffersontimotius@gmail.com>
2024-05-27 10:48:43 +0700

    Daily update

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             DamageTypeSeeder::class,
             DaerahSeeder::class,
+            AnonymousReportSeeder::class,
         ]);
     }
 }
